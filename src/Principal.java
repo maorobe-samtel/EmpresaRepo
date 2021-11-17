9edc2b93be2d69169576e70810fa715e3ebf0779
maorobe14 <51487534+maorobe14@users.noreply.github.com>
2021-11-16 21:26:52 -0500

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello git");
-		System.out.println("segundo guion git");
+		System.out.println("segundo guion git 2");
 
 	}
 
